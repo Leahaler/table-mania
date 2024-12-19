1323a3564cd7ba4cce8ae8bbca570b6126c341a3
Ксеша <leafeon120@gmail.com>
2024-12-19 09:23:30 +0300

    Update tables_text.py

--- tables_text.py
+++ tables_text.py
@@ -1,6 +1,5 @@
 def save_table(table, file_name):
     """Сохраняет таблицу в текстовый файл."""
-    try:
-        with open(file_name, 'w', encoding='utf-8') as f:
-            for row in table:
-                f.write('\t'.join(map(str, row)) + '\n')
+    with open(file_name, 'w', encoding='utf-8') as f:
+        for row in table:
+            f.write('\t'.join(map(str, row)) + '\n')
